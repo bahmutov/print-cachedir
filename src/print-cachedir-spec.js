9622ee2ef80aa92ad6662bf78ff57261177fd864
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-06-01 09:24:08 -0400

    initial code

---
+++ src/print-cachedir-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const printCachedir = require('.')
+
+describe('print-cachedir', () => {
+  it('write this test', () => {
+    console.assert(printCachedir, 'should export something')
+  })
+})
